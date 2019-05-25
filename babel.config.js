38f93aeface081d0b09a55b353455794043dbc9b
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-05-25 19:31:59 +0100

    `package.json`

---
+++ babel.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  'compact': true,
+  'comments': false,
+  'presets': [
+    [
+      '@babel/env', {
+        useBuiltIns: 'entry',
+        targets: {
+          node: 'current',
+          'browsers': [
+            'last 2 versions'
+          ]
+        },
+        corejs: '3.1.2'
+      }
+    ],
+    '@babel/react'
+  ],
+  'plugins': [
+    '@babel/proposal-export-default-from',
+    '@babel/proposal-do-expressions',
+    '@babel/proposal-export-namespace-from',
+    '@babel/proposal-throw-expressions',
+    [
+      '@babel/proposal-class-properties',
+      {
+        loose: false
+      }
+    ],
+    [
+      'module-resolver', {
+        root: ['./src'],
+        cwd: 'babelrc',
+        'alias': {
+          'react-select-element': './src'
+        }
+      }
+    ]
+  ]
+}
