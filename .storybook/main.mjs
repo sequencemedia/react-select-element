7bfb342102766e2eb78021bdea771351769fcd34
Jonathan Perry <jonathanperry@sequencemedia.net>
2025-01-12 14:54:37 +0000

    Storybook

--- .storybook/main.mjs
+++ .storybook/main.mjs
@@ -1,16 +1,23 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 export default {
   stories: ['../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-interactions',
-    '@storybook/addon-links'
+    '@storybook/addon-links',
+    '@storybook/addon-webpack5-compiler-babel',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: '@storybook/react-webpack5',
     options: {}
   },
-  docs: {
-    autodocs: 'tag'
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
   }
 }
